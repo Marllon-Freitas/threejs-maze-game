08c9570a6f0bd0ef5a49b453aa8deb87e29b8d66
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2025-02-15 17:23:25 -0300

    feat: add basic scene init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
+import { useEffect } from 'react'
+import * as THREE from 'three'
+
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
+import Stats from 'three/examples/jsm/libs/stats.module.js'
+
 function App() {
+  useEffect(() => {
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+    const canvas = document.getElementById('maze-game') as HTMLCanvasElement
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true
+    })
+
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    const geometry = new THREE.BoxGeometry()
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    const cube = new THREE.Mesh(geometry, material)
+
+    const controls = new OrbitControls(camera, renderer.domElement)
+
+    const stats = new Stats()
+    document.body.appendChild(stats.dom)
+
+    camera.position.z = 5
+
+    const animate = function () {
+      scene.add(cube)
+      controls.update()
+
+      renderer.render(scene, camera)
+      window.requestAnimationFrame(animate)
+    }
+
+    animate()
+  }, [])
+
   return (
     <>
-      <h1 className="text-2xl font-bold text-center text-blue-500">Salve</h1>
+      <canvas id="maze-game" />
     </>
   )
 }
